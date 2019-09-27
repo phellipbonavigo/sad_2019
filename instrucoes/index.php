50446c8094e657a86bd5801ee0fe94cac96e5203
Phellip Bonavigo <phebonavigo@gmail.com>
2019-09-27 20:25:01 -0300

    Primeiro Commit

---
+++ instrucoes/index.php
@@ -0,0 +1,25 @@
+<?php 
+$dadosUsuarios = array(
+
+array(
+    "nome" => "Jose",
+    "idade" => 20,
+    "altura" => 150
+    
+
+),
+array (
+    "nome" => "Tonini",
+"idade" => 50,
+"altura" => 160
+
+), 
+
+array(
+"nome" => "Maria",
+"idade" => 20,
+"altura" => 150
+
+)
+);
+?>
